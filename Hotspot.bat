a1811cd0bdfd6484ad02adbc2fc18f47507cf92e
SUNIL YADAV <yadavsunil4796@gmail.com>
2016-10-26 21:38:05 +0530

    Add files via upload

--- Hotspot.bat
+++ Hotspot.bat
@@ -1,5 +1,7 @@
 @echo off
-netsh wlan set hostednetwork mode=allow ssid="Connectify-sky" key=23456789
+::netsh wlan set hostednetwork mode=allow ssid="cxxxx{}::::::::::::::::::::::::>" key=23456789
+::"( . )Y( . )"
+::"( . )( . )"
 netsh wlan start hostednetwork
 cls
 netsh wlan show hostednetwork
---
+++ Stop wampapache64.bat
@@ -0,0 +1,1 @@
+sc stop wampapache64
